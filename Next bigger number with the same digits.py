80da23b3e1294fd71c65cdbaf70c581b2d9cab97
sashaboriskin <32432478+sashaboriskin@users.noreply.github.com>
2019-11-21 13:58:31 +0300

    Update Next bigger number with the same digits.py

--- Next bigger number with the same digits.py
+++ Next bigger number with the same digits.py
@@ -1,37 +1,24 @@
-'''
-12 ==> 21
-513 ==> 531
-2017 ==> 2071
-
-9 ==> -1
-111 ==> -1
-531 ==> -1
-'''
-n = 2017
-s = str(n)
-a = []
-number_str = ''
-number_int = 0
-part_a_to_sort = []
-part_a_before_sort = []
-for i in range(len(s)):
-  a.append(int(s[i]))
-was_break =0
-for i in range(len(a)-2,-1,-1):
-  if a[i] < a[i+1]:
-    a[i], a[-1] = a[-1], a[i]
-    part_a_to_sort = a[i+1:len(a)]
-    part_a_before_sort = a[0:i+1]
-    part_a_to_sort.sort() 
-    a = part_a_before_sort + part_a_to_sort
-    was_break = 1
-    break
-
-for i in range(len(a)):
-  number_str = number_str + str(a[i])
-
-number_int = int(number_str)
-if was_break:
-  print(number_int)
-else:
-  print(-1)
+def next_bigger(n):
+    s = str(n)
+    a = []
+    number_str = ''
+    number_int = 0
+    part_a_to_sort = []
+    part_a_before_sort = []
+    for i in range(len(s)):
+      a.append(int(s[i]))
+      
+    for i in range(len(a)-2,-1,-1):
+      if a[i] < a[i+1]:
+        a[i], a[-1] = a[-1], a[i]
+        part_a_to_sort = a[i+1:len(a)]
+        part_a_before_sort = a[0:i+1]
+        part_a_to_sort.sort() 
+        a = part_a_before_sort + part_a_to_sort
+        for i in range(len(a)):
+          number_str = number_str + str(a[i])
+        number_int = int(number_str)
+        return number_int
+        break
+    else:
+      return -1
