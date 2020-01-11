20ff0ce053ba7554182da0aec0505c86d439650b
sashaboriskin <32432478+sashaboriskin@users.noreply.github.com>
2020-01-11 15:14:26 +0300

    Update reverse_a_text.py

--- OpenEdu/TextFile/reverse_a_text.py
+++ OpenEdu/TextFile/reverse_a_text.py
@@ -1,18 +1,15 @@
-#пока не работает
 fin = open('input (7).txt', 'r')
-string = ''
-for line in fin:
-    string = string + line + ' '
-string = string[:-1]
+lines = fin.read()
+print(lines[::-1])
 
 fin.close()
-print(string[::-1])
+
 #aswer
 '''
-.noitairav rallets elttil htiw ,elbats sraeppa rats ehT 
-.metsyS raloS eht ni tneserp si sa iteC uaT gnidnuorrus tsud hcum sa semit 01 naht 
-erom detceted evah snoitavresbO .stenalp ykcor tsoh ot ylekil ssel eb ot thguoht si os 
-dna tneicifed-latem si iteC uaT .rats esolc ylevitaler a si ti ,metsyS raloS eht morf 
-sraey thgil 21 rednu tsuj fo ecnatsid a tA .ssam s'nuS eht fo %87 tuoba ylno sah ti hguohtla 
+.noitairav rallets elttil htiw ,elbats sraeppa rats ehT
+.metsyS raloS eht ni tneserp si sa iteC uaT gnidnuorrus tsud hcum sa semit 01 naht
+erom detceted evah snoitavresbO .stenalp ykcor tsoh ot ylekil ssel eb ot thguoht si os
+dna tneicifed-latem si iteC uaT .rats esolc ylevitaler a si ti ,metsyS raloS eht morf
+sraey thgil 21 rednu tsuj fo ecnatsid a tA .ssam s'nuS eht fo %87 tuoba ylno sah ti hguohtla
 ,nuS eht ot ralimis yllartceps si taht suteC noitalletsnoc eht ni rats a si iteC uaT
 '''
