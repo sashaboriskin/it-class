3034efdbf71660d89c15969bb5c4204a22e61bea
sashaboriskin <32432478+sashaboriskin@users.noreply.github.com>
2020-02-09 23:25:47 +0300

    Update tolstoy.py

--- OpenEdu/TextFile/tolstoy.py
+++ OpenEdu/TextFile/tolstoy.py
@@ -4,12 +4,23 @@ for i in range(1, 3):
     txt += fin.read()
     fin.close()
 
-string_of_bad_letters = ""
+string_of_bad_letters = ",.;:/_!«»-–?()’…;*„“—№=°"
+array_of_bad_letters = [' ', '\n', '\xa0', '\t', ]
 d = {}
+
 for symb in txt:
     if symb in d.keys():
         d[symb]+=1
     else:
         d[symb] = 1
-print(d)
 
+
+for key in d:
+    if key in string_of_bad_letters:
+        del d[key]
+
+print(d)
+'''
+for key, value in d.items():
+    print(key, ' ', value, sep='')
+'''
